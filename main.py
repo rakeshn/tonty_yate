2786784537fa17fb175968219924fb76bb01291a
Shanil <s_nalakath@dell.com>
2020-03-22 22:58:08 -0500

    Update main.py
    comments to main

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from src.models.deck import Deck, Card
 from src.models.game import Round
 
+# main class for temporary testing
+
 round = Round()
 round.play(Card('Spades', '10'),'p1')
 round.play(Card('Spades', '8'),'p2')
-round.play(Card('Spades', 'Jack'),'p3')
+round.play(Card('Spades', '7'),'p3')
 round.play(Card('Spades', 'Ace'),'p4')
 winner, score = round.end()
 print("{0} won by {1}".format(winner, score))
-
-# deck = Deck(4);
-# deck.shuffle()
-#
-# #
-# points = lambda rank: 3 if rank == 'Jack' else 2 if rank == '9' else 1 if rank == 'Ace' else 1 if rank == '10' else 0
-# score = lambda cards: sum(points(card.rank) for card in cards)
-#
-#
-# for i in range(deck.count):
-#     # print(deck.cards[i])
-#     x = deck.cards[i]
-#     # print(x)
-#     # print(points(x.rank))
-# total = score(deck.cards)
-# print(total)
