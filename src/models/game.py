627f2772d6961f8913e13c68da7ae274d999d479
Shanil <s_nalakath@dell.com>
2020-03-22 22:54:02 -0500

    Added 'Playing Round' functionality
    Added crude functionality for any given "playing round". This class will support actions within a round, who wins current round.
    
    pending functionalities.
    1. Enforce starting suit to determine the winner
    2. Enforce trump suit

--- src/models/game.py
+++ src/models/game.py
@@ -1,5 +1,7 @@
 import uuid
 
+from src.models.deck import RANKS
+
 
 class Game:
     def __init__(self):
@@ -8,3 +10,35 @@ class Game:
         self.deck = []
 
 
+class Round:
+    def __init__(self):
+        self.cards = []
+        self.index_of_trump = 0
+        self.players = []
+
+    def call_trump(self):
+        self.index_of_trump = len(self.cards)
+
+    def play(self, card, player):
+        self.cards.append(card)
+        self.players.append(player)
+
+    def end(self):
+        score = _score(self.cards)
+        winner = self.players[highest_card_index(self.cards, self.index_of_trump)]
+        return winner, score
+
+
+_points = lambda rank: 3 if rank == 'Jack' else 2 if rank == '9' else 1 if rank == 'Ace' else 1 if rank == '10' else 0
+_score = lambda cards: sum(_points(card.rank) for card in cards)
+
+
+def highest_card_index(cards, trump_idx):
+    highest_index = RANKS.index('6')
+    card_index = counter = 0 if trump_idx == 0 else trump_idx
+    for card in cards[trump_idx:]:
+        if highest_index > RANKS.index(card.rank):
+            highest_index = RANKS.index(card.rank)
+            card_index = counter
+        counter += 1
+    return card_index
