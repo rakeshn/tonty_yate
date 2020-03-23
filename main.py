e2264a62cf2c169ddf021be42385edccd06180f5
Shanil <s_nalakath@dell.com>
2020-03-23 00:53:24 -0500

    Trump logic is added
    Added few logics for trump

--- main.py
+++ main.py
@@ -1,12 +1,74 @@
 from src.models.deck import Deck, Card
-from src.models.game import Round
+from src.models.game import Round, Game
 
 # main class for temporary testing
 
-round = Round()
-round.play(Card('Spades', '10'),'p1')
-round.play(Card('Spades', '8'),'p2')
+
+# Basic test:  First player with highest card should win
+game = Game()
+round = Round(game)
+round.play(Card('Spades', 'Jack'),'p1')  # Winner
+round.play(Card('Spades', 'Jack'),'p2')
 round.play(Card('Spades', '7'),'p3')
-round.play(Card('Spades', 'Ace'),'p4')
+round.play(Card('Spades', 'Jack'),'p4')
 winner, score = round.end()
-print("{0} won by {1}".format(winner, score))
+print("Test#1 :: {0} won by {1}".format(winner, score))
+
+
+# Trump test:  player with trump should win. Assuming trump was called in previous game
+game = Game()
+game.call_trump()
+game.trump = Card('Hearts', '6')
+round = Round(game)
+round.play(Card('Spades', 'Jack'),'p1')
+round.play(Card('Spades', 'Jack'),'p2')
+round.play(Card('Hearts', '6'),'p3')  # Winner
+round.play(Card('Spades', 'Jack'),'p4')
+winner, score = round.end()
+print("Test#2 :: {0} won by {1}".format(winner, score))
+
+
+# Trump test:  player with trump should win. Assuming trump was called before the play
+game = Game()
+game.trump = Card('Hearts', '6')
+round = Round(game)
+round.play(Card('Spades', 'Jack'),'p1')
+round.play(Card('Spades', 'Jack'),'p2')
+round.call_trump()
+round.play(Card('Hearts', '6'),'p3')  # Winner
+round.play(Card('Spades', 'Jack'),'p4')
+winner, score = round.end()
+print("Test#3 :: {0} won by {1}".format(winner, score))
+
+# Trump test:  player with trump should win. Assuming no one played trump after calling in current round
+game = Game()
+game.trump = Card('Hearts', '6')
+round = Round(game)
+round.play(Card('Spades', 'Jack'),'p1')  # Winner
+round.play(Card('Spades', 'Jack'),'p2')
+round.play(Card('Hearts', '6'),'p3')
+round.call_trump()
+round.play(Card('Spades', 'Jack'),'p4')
+winner, score = round.end()
+print("Test#4 :: {0} won by {1}".format(winner, score))
+
+
+# Trump test: Trump called in first game. Trump was used only in next round(s)
+game = Game()
+game.trump = Card('Hearts', '6')
+round = Round(game)
+round.play(Card('Spades', 'Jack'),'p1')  # Winner
+round.play(Card('Spades', 'Jack'),'p2')
+round.play(Card('Hearts', '6'),'p3')
+round.call_trump()
+round.play(Card('Spades', 'Jack'),'p4')
+winner, score = round.end()
+print("Test#5a :: {0} won by {1}".format(winner, score))
+
+round = Round(game)
+round.play(Card('Diamonds', 'Jack'),'p1')
+round.play(Card('Hearts', '7'),'p2')  # Winner
+round.play(Card('Diamonds', '6'),'p3')
+round.play(Card('Diamonds', 'Jack'),'p4')
+winner, score = round.end()
+print("Test#5b :: {0} won by {1}".format(winner, score))
--- src/models/game.py
+++ src/models/game.py
@@ -8,16 +8,41 @@ class Game:
         self.players = []
         self.id = str(uuid.uuid4().hex)
         self.deck = []
+        self.trump = None
+        self.trump_called = False
 
+    def assign_trump(self, card):
+        self.trump = card
+
+    def call_trump(self):
+        self.trump_called = True
 
 class Round:
-    def __init__(self):
+    def __init__(self, game):
         self.cards = []
         self.index_of_trump = 0
         self.players = []
+        self.game = game
+
+    def highest_card_index(self):
+        highest_index = 99
+        card_index = counter = 0 # if self.index_of_trump == 0 else self.index_of_trump
+        valid_suit = self.cards[0].suit
+
+        if self.game.trump_called is True and any(self.game.trump.suit in c.suit for c in self.cards[self.index_of_trump:]):
+            valid_suit = self.game.trump.suit
+
+        for card in self.cards:
+            if card.suit is valid_suit and highest_index > RANKS.index(card.rank):
+                highest_index = RANKS.index(card.rank)
+                card_index = counter
+            counter += 1
+        return card_index
 
     def call_trump(self):
         self.index_of_trump = len(self.cards)
+        self.game.call_trump()
+
 
     def play(self, card, player):
         self.cards.append(card)
@@ -25,20 +50,11 @@ class Round:
 
     def end(self):
         score = _score(self.cards)
-        winner = self.players[highest_card_index(self.cards, self.index_of_trump)]
+        winner = self.players[self.highest_card_index()]
         return winner, score
 
-
-_points = lambda rank: 3 if rank == 'Jack' else 2 if rank == '9' else 1 if rank == 'Ace' else 1 if rank == '10' else 0
+_points = lambda rank: 3 if rank == 'Jack' else 2 if rank == '9' else 1 if rank in ['Ace', '10'] else 0
 _score = lambda cards: sum(_points(card.rank) for card in cards)
 
 
-def highest_card_index(cards, trump_idx):
-    highest_index = RANKS.index('6')
-    card_index = counter = 0 if trump_idx == 0 else trump_idx
-    for card in cards[trump_idx:]:
-        if highest_index > RANKS.index(card.rank):
-            highest_index = RANKS.index(card.rank)
-            card_index = counter
-        counter += 1
-    return card_index
+
